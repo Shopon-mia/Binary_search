1e618b2ce798239cde9f8493860e2ecda369f418
shopon mia <shopon5677@gmail.com>
2021-06-30 19:37:35 +0600

    program is ready to run

--- binary_search_item.py
+++ binary_search_item.py
@@ -1,19 +1,19 @@
-def binary_search(list, search_item):
+def binary_search(number_list, search_item):
     low_index = 0
-    high_index = len(list - 1)
+    high_index = len(number_list)-1
 
     while low_index <= high_index:
 
         mid = (low_index + high_index) // 2
         
-        if list[mid] == search_item:
+        if number_list[mid] == search_item:
             print("Value found at index : ", mid)
             return
-        if list[mid] < search_item:
+        if number_list[mid] < search_item:
             low_index = mid + 1
-        if list[mid] > search_item:
+        if number_list[mid] > search_item:
             high_index = mid - 1
     print("OOPS!!... value is not found")
 
-list = [10, 20, 30, 40, 50, 60, 70, 80]
-binary_search(list, search_item=100)
+number_list = [10, 20, 30, 40, 50, 60, 70, 80]
+binary_search(number_list, search_item=100) 
